94a3f88b9eb92c40c18fbc3e4c0ff4c4b41c1ace
elena-smeu <elena.smeu@skyspecs.com>
2021-08-27 10:38:14 +0200

    chuck norris app

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,104 @@
 module Main exposing (..)
+
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (..)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode exposing (Decoder, field, string)
+
+
+-- MAIN
+
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
 
-type Msg = Increment | Decrement
 
+
+-- MODEL
+
+
+type Model
+  = Failure
+  | Loading
+  | Success String
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Loading
+  , getRandomJokes
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = MorePlease
+  | GotJson (Result Http.Error String)
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    MorePlease ->
+        (Loading, getRandomJokes)
 
-    Decrement ->
-      model - 1
+    GotJson result ->
+      case result of
+        Ok fullText ->
+          (Success fullText, Cmd.none)
 
+        Err _ ->
+          (Failure, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+  case model of
+    Failure ->
+        div []
+        [ text "I was unable to load your Chuck Norris Joke."
+        , button [ onClick MorePlease ] [ text "Try Again!"]
+        ]
+
+
+    Loading ->
+      text "Loading..."
+
+    Success joke ->
+        div []
+        [ button [ onClick MorePlease, style "display" "block"] [text "Hahaha, Give me another one!"]
+        ,text joke ]
+
+getRandomJokes : Cmd Msg
+getRandomJokes =
+  Http.get
+    { url = "http://api.icndb.com/jokes/random"
+    , expect = Http.expectJson GotJson jokeDecoder
+    }
+
+-- Decoder
+jokeDecoder: Decoder String
+jokeDecoder =
+    field "value" (field "joke" string)
